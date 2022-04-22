d877a65d519b2462e312ced5010dfa69d008c2f8
muralis <muralisc@gmail.com>
2022-04-22 01:38:00 +0100

    Updated scripts

--- check_and_record.sh
+++ check_and_record.sh
@@ -1,4 +1,5 @@
 TIME=$(date -Is)
+LOG_FILE="/tmp/sleepberry.log"
 SLEEP_MONITOR_RAW="/tmp/sleep-monitor_$TIME.flac"
 SLEEP_MONITOR_MP3="/tmp/sleep-monitor_$TIME.mp3"
 arecord -f S24_3LE -D "sysdefault:CARD=Nano" -c 2 -r 48000 $SLEEP_MONITOR_RAW &
@@ -6,25 +7,15 @@ arecord -f S24_3LE -D "sysdefault:CARD=Nano" -c 2 -r 48000 $SLEEP_MONITOR_RAW &
 # 	-d alsa_input.usb-Blue_Microphones_Yeti_Nano_2109SG0014M8_888-000445040606-00.analog-stereo \
 # 	$SLEEP_MONITOR_RAW &
 # rec -c2 $SLEEP_MONITOR &
-echo "$TIME sleeping for 60 ..." >> /tmp/sleep.log
+echo "$TIME sleeping for 60 ..." >> $LOG_FILE
 sleep 60
 pkill arecord
-ffmpeg -y -i $SLEEP_MONITOR_RAW $SLEEP_MONITOR_MP3 >> /tmp/sleep.log
+ffmpeg -y -i $SLEEP_MONITOR_RAW $SLEEP_MONITOR_MP3 >> $LOG_FILE
 rm $SLEEP_MONITOR_RAW
 MAX_AMP=$(sox $SLEEP_MONITOR_MP3 -n stat 2>&1 | grep "Maximum amplitude" | awk '{print $3}')
-echo "$TIME Max Amplitude= $MAX_AMP" >> /tmp/sleep.log
+echo "$TIME Max Amplitude= $MAX_AMP" >> $LOG_FILE
 
 AMP_THRESHOLD=0.009
 if (( $(echo "$MAX_AMP > $AMP_THRESHOLD" |bc -l) )); then
-  echo "$TIME Over amplitude threshold of $AMP_THRESHOLD" >> /tmp/sleep.log
+  echo "$TIME Over amplitude threshold of $AMP_THRESHOLD" >> $LOG_FILE
 fi
-
-
-SLEEP_SPECTOGRAM="/tmp/sleep-spectogram_$TIME.png"
-sox $SLEEP_MONITOR_MP3 -n spectrogram -z100 -o $SLEEP_SPECTOGRAM
-# 
-# TIME=$(date -Is)
-# SLEEP_REC="/tmp/sleep-$TIME.mp3"
-# rec -c2 $SLEEP_REC &
-# sleep 240
-# pkill rec
--- consolidate_sleep_hourly.sh
+++ consolidate_sleep_hourly.sh
@@ -1,17 +1,33 @@
 
 
-PREV_HOUR=$(dateutils.dadd now -1h -f '%Y-%m-%dT%H')
+PREV_HOUR=$(dateutils.dadd --zone "Europe/London" now -1h -f '%Y-%m-%dT%H')
 
-echo "Consolidating $PREV_HOUR ..."
+LOG_FILE="/tmp/sleepberry.log"
 
-ls /tmp/sleep-monitor_${PREV_HOUR}*mp3 |\
-	sed -E "s/(.*)/file \'\1\'/" > sleep_hour_prev_hour.txt
+echo "Consolidating $PREV_HOUR ..." >> $LOG_FILE
 
-CONSOLIDATED_HOURLY="sleep-consolidated_${PREV_HOUR}.mp3"
-echo "Consolidating into $CONSOLIDATED_HOURLY ..."
+PREV_HOUR_FILE_LIST="/tmp/sleep_prev_hour_file_list.txt"
+find /tmp -maxdepth 1 -ipath "*sleep-monitor_${PREV_HOUR}*mp3" |\
+	sed -E "s/(.*)/file \'\1\'/" > $PREV_HOUR_FILE_LIST
+
+CONSOLIDATED_HOURLY="/tmp/sleep-consolidated_${PREV_HOUR}.mp3"
+echo "Consolidating into $CONSOLIDATED_HOURLY ..." >> $LOG_FILE
+if [[ $(wc -l $PREV_HOUR_FILE_LIST | awk '{print $1}') -eq 0 ]]; then
+	echo "No files to process, exiting"
+	exit
+fi
 ffmpeg \
 	-f concat \
 	-safe 0 \
-	-i sleep_hour_prev_hour.txt \
-	-c copy $CONSOLIDATED_HOURLY
-echo "Created $CONSOLIDATED_HOURLY."
+	-i $PREV_HOUR_FILE_LIST \
+	-c copy $CONSOLIDATED_HOURLY 2>&1 > /tmp/sleep_consolidate_ffmpeg.log
+echo "Created $CONSOLIDATED_HOURLY" >> $LOG_FILE
+
+echo "Delting previous hour files starting with $PREV_HOUR" >> $LOG_FILE
+find /tmp -maxdepth 1 -ipath "*sleep-monitor_${PREV_HOUR}*mp3" -delete
+
+SLEEP_SPECTOGRAM="/tmp/sleep-spectogram_$PREV_HOUR.png"
+sox $CONSOLIDATED_HOURLY -n spectrogram -z100 -o $SLEEP_SPECTOGRAM -t $PREV_HOUR
+
+mv $SLEEP_SPECTOGRAM ~/shared_folders/transfer/
+mv $CONSOLIDATED_HOURLY ~/shared_folders/transfer/
